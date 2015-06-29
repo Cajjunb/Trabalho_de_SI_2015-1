4d23bb0da223eb5ac3b2b55bd777a04a0b0d1ac6
Leandro <leandroferreira@cjr.org.br>
2015-06-29 20:45:05 -0300

    mudei no banco

--- trabalhosi.sql
+++ trabalhosi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2015 at 01:26 AM
+-- Generation Time: Jun 30, 2015 at 01:43 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,14 +23,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ta_questao`
+--
+
+CREATE TABLE IF NOT EXISTS `ta_questao` (
+  `idQuestao` int(11) NOT NULL,
+  `enunciadoQuestao` varchar(500) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `linkQuestao` varchar(500) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_questionario`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_questionario` (
+  `idQuestionario` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeQuestionario` varchar(200) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `DescricaoQuestionario` text CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `fotoQuestionario` varchar(500) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  PRIMARY KEY (`idQuestionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_respostas`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_respostas` (
+  `idResposta` int(11) NOT NULL AUTO_INCREMENT,
+  `conteudoResposta` varchar(500) NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT '0',
+  `fkQuestao` int(11) NOT NULL,
+  PRIMARY KEY (`idResposta`),
+  KEY `fkQuestao` (`fkQuestao`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_usuario`
 --
 
 CREATE TABLE IF NOT EXISTS `tb_usuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
-  `perfilUsuario` varchar(20) NOT NULL,
-  `loginUsuario` varchar(200) NOT NULL,
-  `senhaUsuario` varchar(200) NOT NULL,
+  `perfilUsuario` varchar(20) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `loginUsuario` varchar(200) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
+  `senhaUsuario` varchar(200) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
